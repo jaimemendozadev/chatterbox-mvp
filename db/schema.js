7906266956afe98a5b72d8fea593bd103b1b8796
jaimemendozadev <jaimemendozadev@gmail.com>
2017-03-17 19:01:51 -0700

    Fix bcrypt password hash functionality

--- db/schema.js
+++ db/schema.js
@@ -1,14 +1,35 @@
 //why do I have to require the DB inside the schema?
 const db = require('./db.js');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Schema = mongoose.Schema;
  
-const UserSchema = new Schema({
-    username: String,
+var UsersSchema = new Schema({
+    username: {type: String, unique: true, lowercase: true},
     password: String,
-    email: String
 });
 
 
-module.exports = mongoose.model('User', UserSchema );
 
+UsersSchema.pre('save', function(next) {
+  
+  //user is a specific instance of user model
+  const user = this;
+  
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }      
+      user.password = hash;
+      next();
+    });
+  });
+
+});
+
+
+module.exports = mongoose.model('User', UsersSchema );
